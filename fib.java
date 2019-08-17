7240ee4085b485a505f5b990c387afe884411c96
Aleena Maryam <aleenamaryam@gmail.com>
2019-08-16 21:41:23 -0400

    Update fib.java

--- fib.java
+++ fib.java
@@ -3,7 +3,6 @@ public class fib {
     public static void main(String[] args) {
 	int sum = 0;
 	for(int i = 1, j = 2, k; j <= 4000000L; k = j, j+=i, i = k) {
-	    // System.out.println(i + " " + j);
 	    if(j%2==0) {
 		sum+=j;
 	    }
